0a6739cab7a6f2a049771f65caa2493755db7961
Sebastian R Loguzzo <Sebastian.loguzzo@gmail.com>
2023-04-30 20:51:05 -0300

    fix next dotenv

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,10 @@ module.exports = {
     [
       'module:react-native-dotenv',
       {
-        envName: 'APP_ENV',
         moduleName: '@env',
         path: '.env',
       },
     ],
   ],
-  env: {
-    test: {
-      plugins: [['module:react-native-dotenv', { path: '.env' }]],
-    },
-  },
   presets: ['module:metro-react-native-babel-preset'],
 };
